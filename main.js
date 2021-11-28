032c0b68e4770c46357a202001ef3a8e231d8971
TMinarik00 <tinminarik00@gmail.com>
2021-11-28 21:50:13 +0100

    User menu fix

--- main.js
+++ main.js
@@ -101,12 +101,18 @@ window.onresize = onWIndowResize;
 // Deklaracija varijabli
 const menuClick = document.querySelector(".profile-pic");
 const userMenu = document.querySelector(".userMenu");
+const userMenuBackdrop = document.querySelector("#user-menu-backdrop");
 
 // Funkcije
 menuClick.addEventListener("click", function (e) {
   e.preventDefault();
   userMenu.classList.toggle("d-none");
   userMenu.classList.toggle("d-flex");
+  userMenuBackdrop.classList.toggle('d-flex');
 });
 
-
+userMenuBackdrop.addEventListener('click', function(e){
+  userMenuBackdrop.classList.toggle('d-flex');
+  userMenu.classList.toggle("d-none");
+  userMenu.classList.toggle("d-flex");
+})
